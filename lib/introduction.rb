26e3be066b7766995dd9b4ad837dd670acae6667
Jeff Hernandez <softwarejeffhernandez@gmail.com>
2020-07-07 01:26:20 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,6 +1,4 @@
 def introduction_with_language (name, language)
-  puts "Hi, my name is #{name}, and I am learning to code in #{language}."
+  "Hi, my name is #{name} and I am learning to program in #{language}."
+
 end
-
-
-introduction_with_language(Jeff , Ruby)
